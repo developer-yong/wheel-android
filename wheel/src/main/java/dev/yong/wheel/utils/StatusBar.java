a0ce57b5cace8748467932b5e2a6aac39b083c1e
CoderYong <dev_yong@yeah.net>
2019-05-06 23:50:18 +0800

    update:添加状态栏字体颜色控制

--- wheel/src/main/java/dev/yong/wheel/utils/StatusBar.java
+++ wheel/src/main/java/dev/yong/wheel/utils/StatusBar.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.ColorInt;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,18 +42,26 @@ public class StatusBar {
         //去除 ActionBar 阴影
         if (hasActionBar(activity)) {
             if (activity instanceof AppCompatActivity) {
-                ((AppCompatActivity) activity).getSupportActionBar().setElevation(0);
+                ActionBar actionBar = ((AppCompatActivity) activity).getSupportActionBar();
+                if (actionBar != null) {
+                    actionBar.setElevation(0);
+                }
             } else {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    activity.getActionBar().setElevation(0);
+                    android.app.ActionBar actionBar = activity.getActionBar();
+                    if (actionBar != null) {
+                        actionBar.setElevation(0);
+                    }
                 }
             }
         }
         translucent(activity, false);
-        ViewGroup contentLayout = (ViewGroup) activity.findViewById(android.R.id.content);
+        ViewGroup contentLayout = activity.findViewById(android.R.id.content);
         // 设置Activity layout的fitsSystemWindows
         View child = contentLayout.getChildAt(0);
-        child.setFitsSystemWindows(true);
+        if (child != null) {
+            child.setFitsSystemWindows(true);
+        }
 
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, getHeight(activity));
@@ -66,6 +75,15 @@ public class StatusBar {
      * @param activity 需要设置的activity
      */
     public static void translucent(Activity activity, boolean addBarHeight) {
+        translucent(activity, addBarHeight, false);
+    }
+
+    /**
+     * 设置状态栏透明
+     *
+     * @param activity 需要设置的activity
+     */
+    public static void translucent(Activity activity, boolean addBarHeight, boolean isLight) {
         Window window = activity.getWindow();
         //4.4 全透明状态栏
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -73,10 +91,10 @@ public class StatusBar {
         }
         //5.0 全透明实现
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            int visibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | (isLight && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR : 0);
+            window.getDecorView().setSystemUiVisibility(visibility);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
@@ -85,7 +103,7 @@ public class StatusBar {
             if (hasActionBar(activity)) {
                 barHeight += getActionBarHeight(activity);
             }
-            ViewGroup rootView = (ViewGroup) window.getDecorView().findViewById(android.R.id.content);
+            ViewGroup rootView = window.getDecorView().findViewById(android.R.id.content);
             rootView.setPadding(0, barHeight, 0, 0);
         }
     }
